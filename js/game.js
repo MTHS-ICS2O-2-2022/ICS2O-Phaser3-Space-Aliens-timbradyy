20bf16f59e363c7d042782998f62b2bd79fd283f
timbradyy <124389347+timbradyy@users.noreply.github.com>
2023-05-19 17:30:33 +0000

    done

---
+++ js/game.js
@@ -0,0 +1,54 @@
+/* global Phaser */
+
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Tim Brady
+// Created on: April 2023
+// This is the Phaser3 game configuration file
+
+//import statements
+import SplashScene from "./splashScene.js"
+import TitleScene from "./titleScene.js"
+import MenuScene from "./menuScene.js"
+import GameScene from "./gameScene.js"
+
+//create the new scene
+const splashScene = new SplashScene()
+const titleScene = new TitleScene()
+const menuScene = new MenuScene()
+const gameScene = new GameScene()
+
+/**
+ * Start Phaser Game.
+ */
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: false,
+    },
+  },
+  // set the background color
+  backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // we place it in the middle of the page
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+}
+
+const game = new Phaser.Game(config)
+// console.log(game)
+
+//load scenes
+// note remember any "key" is global and CANNOT be reused
+game.scene.add("splashScene", splashScene)
+game.scene.add("titleScene", titleScene)
+game.scene.add("menuScene", menuScene)
+game.scene.add("gameScene", gameScene)
+
+// the start scene
+game.scene.start("splashScene")
---
+++ js/splashScene.js
@@ -0,0 +1,69 @@
+/* global Phaser */
+
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Tim Brady
+// Created on: April 2023
+// This is the splash scene
+
+/**
+ * This class is the Splash Scene.
+ */
+class SplashScene extends Phaser.Scene {
+  /**
+   * This method is the constructor for the Splash Scene.
+   */
+  constructor() {
+    super({ key: "splashScene" })
+
+    this.splashSceneBackgroundImage = null
+  }
+
+  /**
+   * Can be defined on your own scenes.
+   * This method is called by the scene manager when the scene is started,
+   *  before preload() and create ().
+   * @param {object} data - Any data passed via ScenePlugin.add(). or ScenePlugin.start().
+   */
+  init(data) {
+    this.cameras.main.setBackgroundColor("ffffff")
+  }
+
+  /**
+   * Can be defined on your own scenes.
+   * Use it to load assets.
+   */
+  preload() {
+    console.log("Splash Scene")
+    this.load.image("splashSceneBackground", "./assets/splashSceneImage.png")
+  }
+
+  /**
+   * Can be defined on your own scenes.
+   * Use it to create game objects.
+   * @param {object} data - Any data passed via ScenePlugin.add(). or ScenePlugin.start().
+   */
+  create(data) {
+    this.splashSceneBackgroundImage = this.add.sprite(
+      0,
+      0,
+      "splashSceneBackground"
+    )
+    this.splashSceneBackgroundImage.x = 1920 / 2
+    this.splashSceneBackgroundImage.y = 1080 / 2
+  }
+
+  /**
+   * Should be overridden by your own Scenes.
+   * This method is called once per game step while the scene is active.
+   * @param {number} time - The current time.
+   * @param {number} delta - The delta time in ms since the last frame.
+   */
+  update(time, delta) {
+    if (time > 3000) {
+      this.scene.switch("titleScene")
+    }
+  }
+}
+
+export default SplashScene
